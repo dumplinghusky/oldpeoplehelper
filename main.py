6524ca6fb76c88a528d0cfcb07a5b7493a92a2d8
dumplinghusky <67675594+dumplinghusky@users.noreply.github.com>
2020-10-16 13:52:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import math
+import os
+import random
 
 def main():
+  print("Welcome to old people helper v0")
   while run == True:
-  
+    input1 = input.("Enter the task you would like help with today")
+    
+    
